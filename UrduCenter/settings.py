44f9facdfe051783625790cc324036823d403337
faizan-create <faizanaslam1890@gmail.com>
2021-02-22 20:41:01 +0500

    requirments.txt

--- UrduCenter/settings.py
+++ UrduCenter/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-
+import django_heruko
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +129,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-django_heroku.settings(locals())
+django_heruko.settings(locals())
